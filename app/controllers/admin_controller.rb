4e42647a13b4b98a87dac94e9af5ec6ab373f9c7
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-08-22 18:24:51 +0000

    updated

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,3 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :index
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 end
